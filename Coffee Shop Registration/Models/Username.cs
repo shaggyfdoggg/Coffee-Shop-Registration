237330d063cc09d0785edb543761b7a4fef1d26d
Forrest Verellen <fsverellen@mail.com>
2023-08-09 15:02:00 -0400

    first draft

---
+++ Coffee Shop Registration/Models/Username.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace Coffee_Shop_Registration.Models
+{
+	public class Username
+	{
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public string EmailAddress { get; set; }
+		public string Password { get; set; }
+	}
+}
+
